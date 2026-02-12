eab05fdfb3b67dab2db1cb1db60b9dea7ca4e873
hani12-dotcom <dyawidianie56@gmail.com>
2026-02-12 13:39:08 +0700

    Add files via upload

---
+++ KalkulatorSuhu.php
@@ -0,0 +1,36 @@
+<?php
+
+class KalkulatorSuhu {
+    public float $suhu; // dalam Celsius
+
+    // Celsius ke Fahrenheit
+    public function cToF(): float {
+        return ($this->suhu * 9/5) + 32;
+    }
+
+    // Celsius ke Kelvin
+    public function cToK(): float {
+        return $this->suhu + 273.15;
+    }
+}
+
+
+// ================== DATA ==================
+$kalkulator = new KalkulatorSuhu();
+$kalkulator->suhu = 30;S
+
+
+// ================== OUTPUT ==================
+echo "<pre>";
+
+echo "================ KALKULATOR SUHU ================\n";
+echo "Satuan   : Celsius (°C)\n";
+echo "-------------------------------------------------\n";
+echo "SUHU (C)   : " . $kalkulator->suhu . "\n";
+echo "FAHRENHEIT : " . $kalkulator->cToF() . "\n";
+echo "KELVIN     : " . $kalkulator->cToK() . "\n";
+echo "=================================================\n";
+
+echo "</pre>";
+
+?>
